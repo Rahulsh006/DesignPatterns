77e99c841e90388322bf5136fbbf52742c357e06
RAHUL S H <85486874+Rahulsh006@users.noreply.github.com>
2024-10-15 12:28:47 +0530

    Update Demo.java

--- adapter/example/Demo.java
+++ adapter/example/Demo.java
@@ -18,7 +18,7 @@ public class Demo {
         RoundHole hole = new RoundHole(5);
         RoundPeg rpeg = new RoundPeg(5);
         if (hole.fits(rpeg)) {
-            System.out.println("Round peg r5 fits round hole r5.");
+            System.out.println("Round peg r5 fits round hole r55.");
         }
 
         SquarePeg smallSqPeg = new SquarePeg(2);
